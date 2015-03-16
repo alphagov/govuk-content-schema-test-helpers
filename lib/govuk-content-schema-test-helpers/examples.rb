e167becf49ec34e1f14b21f93e6120331849860e
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-03-16 15:36:17 +0000

    Change variable name for clarity
    Rather than have a "schema_name" and "format" as concepts, let's just use
    "format".

--- lib/govuk-content-schema-test-helpers/examples.rb
+++ lib/govuk-content-schema-test-helpers/examples.rb
@@ -4,8 +4,8 @@ module GovukContentSchemaTestHelpers
       Util.check_govuk_content_schemas_path!
     end
 
-    def get(schema_name, example_name)
-      path = example_path(schema_name, example_name)
+    def get(format, example_name)
+      path = example_path(format, example_name)
       check_example_file_exists!(path)
       File.read(path)
     end
@@ -17,9 +17,9 @@ module GovukContentSchemaTestHelpers
     end
 
   private
-    def example_path(schema_name, example_name)
+    def example_path(format, example_name)
       schema_type = GovukContentSchemaTestHelpers.configuration.schema_type
-      File.join(Util.govuk_content_schemas_path, "/formats/#{schema_name}/#{schema_type}/examples/#{example_name}.json").to_s
+      File.join(Util.govuk_content_schemas_path, "/formats/#{format}/#{schema_type}/examples/#{example_name}.json").to_s
     end
   end
 end
