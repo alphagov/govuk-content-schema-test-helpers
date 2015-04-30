751c53767d21377cb2ed9e83aa513a5221ea3bfb
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-04-30 10:47:40 +0000

    Use fixtures for examples spec
    Up until now, the tests ran against the real `govuk-content-schemas`
    repository. This was ok, but we'd now like to evolve the folder structure and
    we would like to be able to test this in isolation from the real files. This
    will allow us to update this gem to be compatible with both folder structures,
    and so allow us to iterate towards the new structure safely.
    
    It's also generally good practice to have tests isolated from other factors
    which may change.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe GovukContentSchemaTestHelpers::Examples do
   let(:subject) { GovukContentSchemaTestHelpers::Examples }
+  let(:fixture_path) { File.join(File.dirname(__FILE__), 'fixtures', 'govuk-content-schemas') }
 
   describe '#initialize' do
     describe 'when the govuk-content-schemas directory does not exist' do
@@ -21,72 +22,55 @@ describe GovukContentSchemaTestHelpers::Examples do
     end
   end
 
-  describe '#get' do
-    before do
-      GovukContentSchemaTestHelpers.configuration.project_root = File.join(File.dirname(__FILE__), '..')
+  context 'when govuk-content-schemas directory does exist' do
+    around do |example|
+      old_path = ENV['GOVUK_CONTENT_SCHEMAS_PATH']
+      ENV['GOVUK_CONTENT_SCHEMAS_PATH'] = fixture_path
       GovukContentSchemaTestHelpers.configuration.schema_type = 'frontend'
-    end
 
-    after do
-      GovukContentSchemaTestHelpers.configuration.project_root = nil
+      example.call
+
+      ENV['GOVUK_CONTENT_SCHEMAS_PATH'] = old_path
       GovukContentSchemaTestHelpers.configuration.schema_type = nil
     end
 
-    describe 'when the example does exist' do
-      it 'loads and parses the example file from govuk-content-schemas' do
-        example = subject.new.get('finder', 'contacts')
-        parsed_example = JSON.parse(example)
-        expect(parsed_example["base_path"]).to eql("/government/organisations/hm-cheese-biscuits/contact")
+    describe '#get' do
+      describe 'when the example does exist' do
+        it 'loads and parses the example file from govuk-content-schemas' do
+          example = subject.new.get('minidisc', 'my_example')
+          parsed_example = JSON.parse(example)
+          expect(parsed_example["base_path"]).to eql("/my-minidisc-example")
+        end
+      end
+
+      describe 'when the example does not exist' do
+        it 'loads and parses the example file from govuk-content-schemas' do
+          expect { subject.new.get('made-up', 'or-a-typo') }.to raise_error(GovukContentSchemaTestHelpers::ImproperlyConfiguredError, /Example file not found/)
+        end
       end
     end
 
-    describe 'when the example does not exist' do
-      it 'loads and parses the example file from govuk-content-schemas' do
-        expect { subject.new.get('made-up', 'or-a-typo') }.to raise_error(GovukContentSchemaTestHelpers::ImproperlyConfiguredError, /Example file not found/)
+    describe '#get_all_for_format' do
+      it 'returns all examples for the format in an array' do
+        examples = subject.new.get_all_for_format('minidisc')
+        expect(examples).to be_an(Array)
+        expect(examples.size).to eq(1)
+        examples.each do |example|
+          parsed_example = JSON.parse(example)
+          expect(parsed_example["format"]).to eql("minidisc")
+        end
       end
     end
-  end
 
-  describe '#get_all_for_format' do
-    before do
-      GovukContentSchemaTestHelpers.configuration.project_root = File.join(File.dirname(__FILE__), '..')
-      GovukContentSchemaTestHelpers.configuration.schema_type = 'frontend'
-    end
+    describe '#get_all_for_formats' do
+      it 'returns all examples for the formats in an array' do
+        examples = subject.new.get_all_for_formats(['minidisc'])
+        expect(examples).to be_an(Array)
+        expect(examples.size).to eq(1)
 
-    after do
-      GovukContentSchemaTestHelpers.configuration.project_root = nil
-      GovukContentSchemaTestHelpers.configuration.schema_type = nil
-    end
-
-    it 'returns all examples for the format in an array' do
-      examples = subject.new.get_all_for_format('case_study')
-      expect(examples).to be_an(Array)
-      expect(examples.size).to be >= 2
-      examples.each do |example|
-        parsed_example = JSON.parse(example)
-        expect(parsed_example["format"]).to eql("case_study")
+        formats_returned = examples.map { |e| JSON.parse(e)['format'] }.uniq
+        expect(formats_returned).to match_array(['minidisc'])
       end
-    end
-  end
-
-  describe '#get_all_for_formats' do
-    before do
-      GovukContentSchemaTestHelpers.configuration.project_root = File.join(File.dirname(__FILE__), '..')
-      GovukContentSchemaTestHelpers.configuration.schema_type = 'frontend'
-    end
-
-    after do
-      GovukContentSchemaTestHelpers.configuration.project_root = nil
-      GovukContentSchemaTestHelpers.configuration.schema_type = nil
-    end
-
-    it 'returns all examples for the formats in an array' do
-      examples = subject.new.get_all_for_formats(['case_study', 'finder'])
-      expect(examples).to be_an(Array)
-      expect(examples.size).to be >= 2
-
-      formats_returned = examples.map { |e| JSON.parse(e)['format'] }.uniq
-      expect(formats_returned).to match_array(['case_study', 'finder'])
     end
   end
 end
